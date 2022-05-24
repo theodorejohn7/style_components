9dc38e7221caab226068441d3b4e735edf5910ed
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-05-24 16:09:17 +0530

    home page completed

---
+++ src/components/Features/Features.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { Container, Section } from "../../globalStyles";
+import {
+  FeatureText,
+  FeatureTitle,
+  FeatureWrapper,
+  FeatureColumn,
+  FeatureImageWrapper,
+  FeatureName,
+  FeatureTextWrapper,
+} from "./Features.Styles";
+
+import { featuresData } from "../../data/FeaturesData";
+
+const Features = () => {
+  const initial = {
+    y: 40,
+    optacity: 0,
+  };
+
+  const animate = {
+    y: 0,
+    opacity: 1,
+  };
+
+  return (
+    <Section smPadding="50px 10px" position="relative" inverse id="about">
+      <Container>
+        <FeatureTextWrapper>
+          <FeatureWrapper>
+            {featuresData.map((el, index) => (
+              <FeatureColumn
+                initial={initial}
+                animate={animate}
+                transition={{ duration: 0.5 + index * 0.1 }}
+                key={index}
+              >
+                <FeatureImageWrapper className={el.imgClass}>
+                  {el.icon}
+                </FeatureImageWrapper>
+                <FeatureName>{el.name}</FeatureName>
+                <FeatureText>{el.description}</FeatureText>
+              </FeatureColumn>
+            ))}
+          </FeatureWrapper>
+        </FeatureTextWrapper>
+      </Container>
+    </Section>
+  );
+};
+
+export default Features;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
+import Features from "../components/Features/Features";
 import Hero from "../components/Hero/Hero";
+import {heroOne, heroTwo, heroThree} from "../data/HeroData"
+import {Content} from '../components/Content/Content'
 
 function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Content {...heroOne} />
+      <Content {...heroTwo} />
+      <Content {...heroThree} />
+
+    
+    </>
+  );
 }
 
 export default Home;
