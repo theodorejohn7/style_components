03fae676853dd3a00236d867810729fb9a5951ee
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-05-24 14:07:12 +0530

    navlink issue fixed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaTimes } from "react-icons/fa";
+import { FaRProject, FaTimes } from "react-icons/fa";
 import { CgMenuRight } from "react-icons/cg";
 import { IconContext } from "react-icons";
 import {
@@ -17,53 +17,52 @@ import { useState } from "react";
 
 import { useLocation, useNavigate } from "react-router-dom";
 
-import data from "../../data/NavbarData";
+import { data } from "../../data/NavbarData";
 
 const Navbar = () => {
+  const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false);
+  let history = useNavigate();
+  let location = useLocation();
 
-    let history = useNavigate();
-    let location = useLocation();
+  const handleClick = () => {
+    setShow(!show);
+  };
 
+  const scrollTo = (id) => {
+    const element = document.getElementById(id);
 
-    const handleClick=()=>{
-        setShow(!show);
+    element.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const closeMobileMenu = (to, id) => {
+    if (id && location.pathname === "/") {
+      scrollTo(id);
     }
-
-    const scrollTo= (id)=>{
-        const element = document.getElementById(id);
-
-        element.scrollIntoView({
-            behavior:'smooth',
-        })
-    }
-
-    const closeMobileMenu = (to,id) => {
-        if(id && location.pathname ==='/'){
-            scrollTo(id);
-        }
-        history.push(to);
-        setShow(false);
-    }
-
+    history.push(to);
+    setShow(false);
+  };
 
   return (
-    <IconContext.Provider value={{color:"#fff"}}>
+    <IconContext.Provider value={{ color: "#fff" }}>
       <Nav>
         <NavbarContainer>
-          <NavLogo to='/'>
+          <NavLogo to="/">
             <NavIcon src="./assets/logo.png" alt="logo" />
-              Delta
+            Delta
           </NavLogo>
 
-          <MobileIcon onClick={handleClick}>{show ? <FaTimes /> : <CgMenuRight />} </MobileIcon>
-          <NavMenu>
+          <MobileIcon onClick={handleClick}>
+            {show ? <FaTimes /> : <CgMenuRight />}
+          </MobileIcon>
+          <NavMenu show={show}>
             {data.map((el, index) => (
               <NavItem key={index}>
-                <NavLinks onClick={()=> closeMobileMenu(el.to,el.id)}  >
-                    {el.text}
-                    </NavLinks>
+                <NavLinks onClick={() => closeMobileMenu(el.to, el.id)}>
+                  {el.text}
+                </NavLinks>
               </NavItem>
             ))}
           </NavMenu>
--- src/data/NavbarData.js
+++ src/data/NavbarData.js
@@ -15,5 +15,3 @@ export const data = [
   },
 ];
 
-
-export default data;
