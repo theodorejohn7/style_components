5d0cc9358935d9f6e406884d07a6e75f1bcb7d73
theodorejohn7 <theodorejohn.s@10decoders.in>
2022-05-24 12:16:08 +0530

    home page v1 compmleted

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import SignupPage from "./pages/SignupPage";
+import PricingPage from "./pages/PricingPage";
 
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <Router>
+      <GlobalStyle />
+      
+      <Routes>
+        <Route path="/"  exact  element={<Home />} />
+        <Route path="/signup" exact element={<SignupPage />} />
+        <Route path="/pricing" exact element={<PricingPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Container, MainHeading } from "../../globalStyles";
+import {
+  HeroVideo,
+  HeroSection,
+  HeroText,
+  ButtonWrapper,
+  HeroButton,
+} from "./HeroStyles";
+
+export default function Hero() {
+  return (
+    <HeroSection>
+      <HeroVideo src="./assets/hero.mp4" />
+
+      <Container>
+        <MainHeading> Your data is secure with us</MainHeading>
+
+        <HeroText>
+          We provide the best security systems for clients all over the world
+        </HeroText>
+        <ButtonWrapper>
+          <Link to="signup">
+            <Button>Get Started</Button>
+          </Link>
+          <HeroButton>Find More</HeroButton>
+        </ButtonWrapper>
+      </Container>
+    </HeroSection>
+  );
+}
